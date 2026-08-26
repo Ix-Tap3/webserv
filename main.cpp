75b46eac2fea8abf02e517018f29539ad98ac90f
pcaplat <pcaplat@student.42angouleme.fr>
2026-08-26 11:40:14 +0200

    add a simple main

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcaplat </var/spool/mail/pcaplat>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/26 11:36:34 by pcaplat           #+#    #+#             */
+/*   Updated: 2026/08/26 11:38:07 by pcaplat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main( int ac, char **av )
+{
+	if (ac != 2)
+	{
+		std::cerr << "Error: please use: ./webserv <configuration_file>." << std::endl;
+		return 1;
+	}
+	std::cout << av[1] << std::endl;
+}
